d675c1f9864d11bf96842f3dce69315660e86c2f
Ayaaz Street <astreet@dvtsoftware.com>
2024-10-18 11:16:01 +0200

    feat: :truck: Adding item-details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'item/:itemId',
+        loadComponent: () => import('./item-details/item-details.component').then(c => c.ItemDetailsComponent)
+    }
+];
